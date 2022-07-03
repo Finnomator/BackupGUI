7fa60969a0eb442ad2e02e1d1af7de847ad0d09e
Finn Drünert <finndruenert@gmail.com>
2022-07-03 08:56:34 +0200

    Init

---
+++ Backup/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinForms = System.Windows.Forms;
+
+namespace Backup
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Label l = new();
+            l.Content = "Y";
+            Label b = new();
+            b.Content = "b";
+            PathsList.Items.Add(l);
+            PathsList.Items.Add(b);
+
+        }
+
+        private void SelectNewBackupPath(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog;
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            WinForms.DialogResult result = folderDialog.ShowDialog();
+        }
+    }
+}
